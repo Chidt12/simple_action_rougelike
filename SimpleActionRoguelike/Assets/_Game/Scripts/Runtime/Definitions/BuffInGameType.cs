8979a36cfa20603bd1e2d283b607a92c02a74998
chinm <vipmath171@gmail.com>
2023-06-19 20:58:06 +0700

    add some log.

--- SimpleActionRoguelike/Assets/_Game/Scripts/Runtime/Definitions/BuffInGameType.cs
+++ SimpleActionRoguelike/Assets/_Game/Scripts/Runtime/Definitions/BuffInGameType.cs
@@ -1,15 +1,16 @@
-namespace Runtime.Definition
+﻿namespace Runtime.Definition
 {
-    public enum BuffInGameType
+    public enum BuffInGameType // Có những item sẽ được spawn ngẫu nhiên ở trên map
     {
         None = 0,
-        RotateOrbs = 1,
-        UpgradeWeapon = 2,
-        AutoProjectiles = 3,
-        HealthRecover = 4, 
-        Invincible = 5,
-        FreezeArea = 6,
-        MiniHelper = 7,
-        StableGun = 8,
+        RotateOrbs = 1, // trigger will attack nearly target, giết 1 quái thì được hồi 1 cầu, trigger thì bắn cầu đi => item sẽ có thử tự ưu tiên trigger ví dụ như quả cầu này sẽ luôn là least.
+        UpgradeWeapon = 2, // upgrade ưeapon
+        AutoStableGun = 3, // nhặt and trigger
+        FreezeArea = 4, // nhặt and trigger
+        LightningStorm = 5, // nhặt and trigger
+        BuffAllStats = 6, // nhặt and trigger
+        CreateBuffFlag = 7, // nhặt and trigger
+        AutoRobot = 8, // nhặt and trigger
+        Boomerang = 9, // nhặt and trigger
     }
 }
