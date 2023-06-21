44f996f32ecd399f5d507475c1f9bc7c4c13e10e
chidt12 <vipmath171@gmail.com>
2023-06-21 11:21:20 +0700

    Add template artifact

--- SimpleActionRoguelike/Assets/_Game/Scripts/Runtime/Definitions/ArtifactType.cs
+++ SimpleActionRoguelike/Assets/_Game/Scripts/Runtime/Definitions/ArtifactType.cs
@@ -6,11 +6,12 @@
         RotateOrbs = 1, // trigger will attack nearly target, giết 1 quái thì được hồi 1 cầu, trigger thì bắn cầu đi => item sẽ có thử tự ưu tiên trigger ví dụ như quả cầu này sẽ luôn là least.
         UpgradeWeapon = 2, // upgrade ưeapon
         AutoStableGun = 3, // nhặt and trigger
-        FreezeArea = 4, // nhặt and trigger
-        LightningStorm = 5, // nhặt and trigger
-        BuffAllStats = 6, // nhặt and trigger
-        CreateBuffFlag = 7, // nhặt and trigger
-        AutoRobot = 8, // nhặt and trigger
-        Boomerang = 9, // nhặt and trigger
+        StatusForwardArea = 4, // nhặt and trigger
+        StatusStorm = 5, // nhặt and trigger
+        StatusAroundArea = 6, // nhặt and trigger
+        BuffAllStats = 7, // nhặt and trigger
+        CreateBuffFlag = 8, // nhặt and trigger
+        AutoRobot = 9, // nhặt and trigger
+        Boomerang = 10, // nhặt and trigger
     }
 }
